b36aa0ed53a0355bb0cd705765f3ba0bd350c4c1
bhupendra <bhupendra.sisodia806@gmail.com>
2023-12-29 20:34:04 +0530

    updated

--- todolist/todolist/settings/prod.py
+++ todolist/todolist/settings/prod.py
@@ -1,16 +1,1 @@
 
-# from .base import *     # noqa
-
-# DEBUG = False
-
-# # Production MySQL Database Configuration
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'prod_db',
-#         'USER': 'prod_user',
-#         'PASSWORD': 'prod_password',
-#         'HOST': 'production-db-server',
-#         'PORT': '3306',
-#     }
-# }
